7f2939195b1c94d2c7ef6c5bcfa8bb88c04bfa09
idea3dsvk <idea3dsvk@gmail.com>
2025-10-30 22:30:40 +0100

    Add environment files for Vercel deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+// Production Firebase Configuration
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBqisPaUxduzxorz0tD23PmgnDSI8Q_WvY",
+    authDomain: "kalibracie-d10e5.firebaseapp.com",
+    projectId: "kalibracie-d10e5",
+    storageBucket: "kalibracie-d10e5.firebasestorage.app",
+    messagingSenderId: "163437534404",
+    appId: "1:163437534404:web:89134beca05a2ca2c47d91"
+  },
+  recaptcha: {
+    siteKey: "YOUR_RECAPTCHA_V3_SITE_KEY"
+  }
+};
